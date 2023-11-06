7cc33aeb08764b0955d2984587adfd64ff5cfd1c
oceane <oceane.copub@gmail.com>
2023-11-06 19:54:11 +0100

    fin

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -1,6 +1,7 @@
 package agenda;
 
 import java.time.*;
+import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
@@ -9,18 +10,25 @@ import java.util.ArrayList;
  */
 public class RepetitiveEvent extends Event {
     private ChronoUnit frequency;
-    private ArrayList<LocalDate> exceptions= new ArrayList<LocalDate>();
+    private ArrayList<LocalDate> exceptions = new ArrayList<LocalDate>();
 
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        this.frequency=frequency;
+        this.frequency = frequency;
 
     }
 
     @Override
     public boolean isInDay(LocalDate aDay) {
         if (exceptions.contains(aDay))
-                return false;
+            return false;
+        LocalDate dateTest = LocalDate.from(getStart());
+        while (dateTest.isBefore(ChronoLocalDate.from(aDay.atStartOfDay())) || dateTest.equals(aDay)) {
+            if (dateTest.plus(1, frequency).equals(aDay)) {
+                return true;
+            }
+            dateTest=dateTest.plus(1, frequency);
+        }
         return super.isInDay(aDay);
     }
 
